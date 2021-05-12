a0fb59cacc64f78272c949558df16c7d497d4a93
R.Phuwanat <ggamegg170@gmail.com>
2021-05-13 00:37:07 +0700

    Dynamic Tile Load alpha

--- Assets/Scripts/Track/TrackController.cs
+++ Assets/Scripts/Track/TrackController.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class TrackController : MonoBehaviour
 {
-   
+  public GameObject[] Tile;
+  public float TileLoadDelay = 1f;
+  private Vector3 LastTilePosition = new Vector3(0,0,0);
+  private float Timer;
+  private void Update()
+  {
+    if (Game.GameStarted && !Game.Over)
+    {
+      Timer += Time.deltaTime;
+      if (Timer >= TileLoadDelay)
+      {
+        Instantiate(Tile[0], LastTilePosition += Vector3.forward * 10, Quaternion.identity);
+        Timer = 0;
+      }
+    }
+  }
 }
