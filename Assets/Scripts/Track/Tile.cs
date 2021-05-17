6523e7b2c8871c7a2dd33dd00cbb091052681717
R.Phuwanat <ggamegg170@gmail.com>
2021-05-17 16:04:06 +0700

    Coin Spin

--- Assets/Scripts/Track/Tile.cs
+++ Assets/Scripts/Track/Tile.cs
@@ -11,13 +11,21 @@ public class Tile : MonoBehaviour
   public float CoinLaneOffset = 0;
   public float CoinPositionXOffset;
   private int[] Lanes = new[] { -7, 0, 7 };
+  private GameObject CoinSet;
   void Start()
   {
+    StartCoroutine(SpawnCoin());
+  }
+  private IEnumerator SpawnCoin()
+  {
     int Len = GetRandomLanes();
+    CoinSet = new GameObject("CoinSet");
+    CoinSet.transform.parent = this.gameObject.transform;
     for (int i = 0; i <= 5; i++)
     {
-      var ChildCoin = Instantiate(CoinModel, new Vector3(Len - CoinLaneOffset, CoinPositionXOffset, transform.position.z + (i * 10)), CoinRotation);
-      ChildCoin.transform.parent = this.gameObject.transform;
+      var ChildCoin = Instantiate(CoinModel, new Vector3(Len - CoinLaneOffset, CoinPositionXOffset, transform.position.z + (i * 10)), Quaternion.identity);
+      ChildCoin.transform.parent = CoinSet.transform;
+      yield return new WaitForSeconds(1f);
     }
   }
   private int GetRandomLanes()
