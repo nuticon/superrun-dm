83cddbba4594c617f523d02c4ac3984bd9ae064c
R.Phuwanat <r.phuwanat.dev@gmail.com>
2021-08-20 22:17:57 +0700

    floating market spawn

--- Assets/Scripts/Track/Landmark.cs
+++ Assets/Scripts/Track/Landmark.cs
@@ -2,14 +2,4 @@ using UnityEngine;
 
 public class Landmark : MonoBehaviour
 {
-  void Update()
-  {
-    if (Game.GameStarted && !Game.Over && Game.CountDownEnded) Move();
-  }
-  void Move()
-  {
-    // Vector3 TargetPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + Character.Position.z);
-    // transform.position = Vector3.Lerp(transform.position, TargetPosition, Time.deltaTime * CharacterMovement.Speed * CharacterMovement.SpeedMultiplier);
-    transform.position += Vector3.forward * Time.deltaTime * CharacterMovement.Speed * CharacterMovement.SpeedMultiplier;
-  }
 }
