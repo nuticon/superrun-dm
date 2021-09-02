199d9c909a4efea572342ee64ffbe42d496a2163
R.Phuwanat <r.phuwanat.dev@gmail.com>
2021-09-02 14:39:47 +0700

    Init Shop Ui

---
+++ Assets/Scripts/Game/ShopUi.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUi : MonoBehaviour
+{
+  public Text MagnetPriceText;
+  public Text DoublePriceText;
+  public Text LifeText;
+  public GameObject MagnetLevelBar;
+  public GameObject DoubleLevelBar;
+  public GameObject LifeLevelBar;
+}
